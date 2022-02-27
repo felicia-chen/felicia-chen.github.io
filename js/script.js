542ba4b4a6e8446f2e3d17e696ace03c097bfba4
felicia-chen <31674311+felicia-chen@users.noreply.github.com>
2022-02-26 22:34:16 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-//Responsive Menu
-
-
-//Page Transition Fades Script
-// window.addEventListener("beforeunload", function () {
-//   document.body.classList.add("animate-out");
-// });
-
-//masonry
-// $(document).ready(function() {
-//     $(function(){
-//         var $container = $('.masonry');
-//         $container.imagesLoaded( function(){
-//             $container.masonry({
-//                 itemSelector : '.item'
-//             });
-//         });
-//     });
-//
-// });
 
 //click on mobile navigation drawer
 $(document).ready(function() {
@@ -45,38 +25,84 @@ $(document).ready(function() {
     });
 });
 
-//Image Expansion Script
-// var modal = document.getElementById('myModal');
-// var slideIndex = 1;
-// function currentSlide(n) {
-//     slideIndex = n;
-//     var i = 'myImg' + n.toString();
-//     var img = (document.getElementById(i));
-//     var modalImg = document.getElementById("img01");
-//     var captionText = document.getElementById("caption");
-//         modal.style.display = "block";
-//         modalImg.src = img.src;
-//         captionText.innerHTML = img.alt;
-//             var height = img.width;
-//         captionText.style.marginTop = height;
-//
-//         // Get the <span> element that closes the modal
-//         var span = document.getElementsByClassName("close")[0];
-//
-//         // When the user clicks on <span> (x), close the modal
-//         span.onclick = function() {
-//             modal.style.display = "none";
-//         }
-// }
-// // Get the modal
-//
-//
-// //previous slide
-// function previousSlide() {
-//     currentSlide(slideIndex - 1);
-// }
-//
-// //next slide
-// function nextSlide() {
-//     currentSlide(slideIndex + 1).fadeIn(200);
-// }
+
+
+//MODAL SCRIPTS
+//detect click outside of modal; if so, close modal
+$(document).click(function(e){
+    var clickElement = e.target;  // get the dom element clicked.
+    var elementClassName = e.target.className;  // get the classname of the element clicked
+    myModal = document.getElementById("myModal");
+    opacity = window.getComputedStyle(myModal).getPropertyValue("opacity"); //check visibility of modal
+
+    //if clicked on modal or image (not next/prev buttons) and the modal is visible, close modal
+    if (elementClassName == "slide" && opacity==1) {
+      closeModal();
+    }
+    else if (elementClassName == "modal" && opacity==1) {
+      closeModal();
+    };
+    //alert(elementClassName); check class name
+});
+//arrow keys simulate next and prev
+  document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37:
+            //left
+            plusSlides(-1);
+            break;
+        case 38:
+            //up
+            break;
+        case 39:
+            //right
+            plusSlides(1);
+            break;
+        case 40:
+            //down
+            break;
+        case 27:
+            //esc
+            closeModal();
+            break;
+    }
+  };
+
+  function onClick(element) {
+     $("#myModal").fadeIn(300);
+  }
+
+  function closeModal() {
+      $("#myModal").fadeOut(300);
+  }
+
+  var slideIndex = 1;
+  showSlides(slideIndex, false);
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n, false);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n, true);
+  }
+
+  function showSlides(n, plus) {
+    var bool = plus;
+    var i;
+    var slides =  $(".mySlides");
+    var dots = $(".mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        if (bool)
+   {
+       slides[i].style.display = 'none';
+   }    else {$(slides[i]).fadeOut(200)};
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    $(slides[slideIndex-1]).fadeIn(200);
+    dots[slideIndex-1].className += " active";
+  }
